9ea3d66a98398c01a7529e161b4ed22d8a3e5286
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-01-30 07:58:14 -0800

    Add files via upload

---
+++ hello.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+class hello{
+
+public static void main(String[] args){
+                                     
+									 System.out.println("hello bhailog");
+									 
+									 }
+
+           }
