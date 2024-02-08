abff9edb26f5b0facad22d67619227f81e90a3c9
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-08 14:40:52 +0600

    Add files via upload

---
+++ class2.java
@@ -0,0 +1,19 @@
+public class class2 {
+
+    public class2(){
+
+        System.out.println("hello bhai");
+
+
+    }
+    public void show(){
+        System.out.println("bhai ar partesina !!!");
+    }
+
+
+    public static void main(String [] args){
+        class2 s1 = new class2();
+        s1.show();
+    }
+    
+}
