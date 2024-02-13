1b80a183e199aa63830b3927d212045233dc27ef
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-14 00:29:32 +0600

    Add files via upload

---
+++ Bkash.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+
+
+public class Bkash
+{
+	private String name ;
+	private double amount;
+	
+	public void setname(String name){
+		this.name = name;
+	}
+	public void setamount(double amount){
+	this.amount = amount;
+	
+	}
+	public void transfer(Bkash a ,double amount){
+		
+		this.amount = this.amount - amount ;
+		a . amount = a.amount + amount;
+		
+		
+	}
+	public String getname(){
+	return name ;}
+	public double getamount (){
+	return amount ;}
+	
+		public void display(){
+		
+		System.out.println("name :"+name);
+	    System.out.println("amount :"+amount);	
+		
+	}
+	
+}
