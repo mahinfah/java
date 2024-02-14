4634935f2328061c3f12a7cd1925dbd8245a63b6
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-14 20:56:39 +0600

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+
+
+public class userinput {
+    public static void main(String [] args){
+    System.out.println("enter your name :");
+    Scanner sc = new Scanner(System.in);
+   String m = sc.nextLine();
+    System.out.println("your name :"+m);
+
+
+
+
+}
+}
