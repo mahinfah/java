3862ed7249900855325fcf8b76939be5a9bb064f
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-19 00:25:09 +0600

    Add files via upload

---
+++ string1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class string1 {
+    public static void main(String [] args){
+
+//        Scanner sc = new Scanner(System.in);
+  //      String a = sc.nextLine();
+//        System.out.println(a);
+   
+  String s1 = "hello world.";
+  String s2 = "hi";
+  String s3 ="hi";
+System.out.println(s2==s3);
+  //System.out.println(s2.charAt(0));
+//yyu76System.out.println(s2.indexof("i"));
+}
+
+}
