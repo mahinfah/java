6b185540dbabbab817096ad20cc9b5fc381021ff
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-23 17:00:45 +0600

    Add files via upload

---
+++ charat.java
@@ -0,0 +1,12 @@
+public class charat {
+    public static void main(String[] args){
+    String a ="hi mahin"  ;
+    String b  ="hi mahin";
+    System.out.println(a.charAt(1));
+
+    // finding index
+    System.out.println(a.indexOf("i"));
+
+    System.out.println(a.equals(b));
+}
+}
