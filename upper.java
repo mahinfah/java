bcd106564891d7ebb50ccc9cb476970b25b4546d
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-24 01:40:14 +0600

    Add files via upload

---
+++ upper.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class upper {
+    public static void main(String[] arg){
+String a = "hello";
+System.out.println(a.toUpperCase());
+Scanner sc = new Scanner(System.in);
+
+
+String m = sc.nextLine();
+System.out.println(m.toUpperCase());
+
+
+    }
+
+    }
+    
