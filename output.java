d4dc71a533b1bdda3a499078081fe2cb9e05279e
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-02-28 09:59:38 -0800

    Update output.java

--- output.java
+++ output.java
@@ -11,17 +11,16 @@ public class output{   // main function test.java
 	 System.out.println("testing");
 	 
 	 }
-		 public output(int a ,int s){
-this.a = a;
-			 
-	b=b+10;
-arr = new String[s];
-	System.out.println("cp  is   fun");
+	 public output(int a ,int s){
+       this.a = a;
+       b=b+10;
+       arr = new String[s];
+      System.out.println("cp  is   fun");
 		 }
 		  
 		  static {
 			  System.out.println("if understand!");
-           b =30;			  
+                          b =30;			  
 		  }
 		  
 		  public void StringOP(String s1,String s2)
@@ -54,19 +53,19 @@ arr = new String[s];
 
 import java.lang.*;
 public class test{
-public static void main(String[] args){
-	output t1 = new output(2,3);
-	output t2= new output();
-	System.out.println(t1.a);
-	System.out.println(t2.a);
-	System.out.println(t1.b);
-	System.out.println(t2.b);
-	t1.StringOP("Hello","Hi");
-t2.arr2demo();}
-	*/
-
-
-
+public static void main(String[] args){           OUTPUT:
+	output t1 = new output(2,3);              if understand
+	output t2= new output();                  cp is fun
+	System.out.println(t1.a);                 testing
+	System.out.println(t2.a);                  2
+	System.out.println(t1.b);                  0
+	System.out.println(t2.b);                  20
+	t1.StringOP("Hello","Hi");                 20
+        t2.arr2demo();}                             5 (length of array 0 )         
+	                                            3(length of array)
+                                                   hellohi
+                                                    23
+                                                    15   */
 
 
 
