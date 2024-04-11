1b00df6657644c2922c3c6a4a5555c6f08900676
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-04-11 09:40:46 -0800

    Update portal.java

--- portal by swing/portal.java
+++ portal by swing/portal.java
@@ -1,72 +1,113 @@
-import javax.swing.*;
 
+import java.lang.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class portal
+//import java.nio.file.*;
+
+
+public class portal implements ActionListener
 {
 	private JFrame f;
-	private JButton b;
-	private JTextField t;
-	private JLabel l;
+	private JButton b ,login2;
+	private JTextArea access;
+	private JTextField id;
+	private JLabel l ;
+	private Cursor cursor;
 	 private Container c;
+//	 private JButton login2;
 	 private ImageIcon logo ,login;
-	
+	private JPasswordField pass ;
 	public portal()
 	{
-		
-		
-		f=new JFrame("Index");
 		//f.getContentPane().setBackground(Color.orange);
-		  c = f.getContentPane();
-      //  c.setBackground(Color.decode("#800080"));
-		//sum=new JButton("Summation(+)");		       // ImageIcon logo = new ImageIcon("");
-
-		        ImageIcon logo = new ImageIcon("framepic.png");
-        f.setIconImage(logo.getImage());
-        f.setLocationRelativeTo(null);
 		
- ImageIcon login = new ImageIcon("hh.png");
+		 f=new JFrame("Index");
+		 c = f.getContentPane();
+          pass = new JPasswordField();
+		  ImageIcon logo = new ImageIcon("framepic.png");
+          f.setIconImage(logo.getImage());
+        //  f.setLocationRelativeTo(null);
+		  id = new JTextField();
+		  // Color color=new Color(255,0,0);  this two line for cng text color
+		   // id.setForeground(color);  
+		 		access = new JTextArea("Can't access your account?");
+		// t.setBackground(new Color(51,204,255));
+		 Color color=new Color(51,153,255);
+		  access.setForeground(color);
+		 
+        ImageIcon login = new ImageIcon("hh.png");
         JLabel j = new JLabel(login);
         j.setBounds(1, 1, login.getIconWidth(), login.getIconHeight());
         f.add(j);
-l = new JLabel("Sign in with your organizational id number.");
-l.setBounds(86,160,380,40);
-
-		//minus=new JButton("Minus(-)");
-//		mult=new JButton("Multiply(*)");
-//		div=new JButton("Division(/)");
-		
-//		num1= new JTextField();
-//		num2= new JTextField();
-//		solution= new JTextField();
-		
-//		num1l=new JLabel("Number-1");
-//		num2l=new JLabel("Number-2");
-//		solutionl=new JLabel("Solution");
-		
-		f.setSize(415,415);
-		
-//*num1l.setBounds(100,50,100,100);
-	//	num2l.setBounds(300,50,100,100);
-	//	num1.setBounds(100,200,100,100);
-	//	num2.setBounds(300,200,100,100);
-	///	sum.setBounds(100,400,100,100);
-		//minus.setBounds(250,400,100,100);
-	f.add(l);
+			
+         ImageIcon huh = new ImageIcon("login.png");
+         login2 = new JButton(huh);
+         login2.setBounds(1, 1, huh.getIconWidth(), huh.getIconHeight());
+		//login2.setBackground(new Color(0, 0, 0, 0));
+       // login2.setOpaque(false);
+       // login2.setBorder(BorderFactory.createEmptyBorder());
+      //login2.setCursor(cursor);
+		login2.setBounds(25,350,365,40);
+	  l = new JLabel("Sign in with your organizational id number.");
+        l.setBounds(86,160,380,40);
+        id.setBounds(25,230,365,40);
+	    pass.setBounds(25,280,365,40);
+       // login.setBounds(
+		access.setBounds(70,420,300,40);
+		access.setBackground(new Color(255, 255, 255));
+		f.setSize(415,515);
+		f.add(l);
+	    f.add(pass);
+		f.add(login2);
+		f.add(access);	
 		f.setLayout(null);
 		
+		f.add(id);
 		f.setVisible(true);
+	
+		// login2.addActionListener(this);
+		 login2.addActionListener(this);
+		 access.addActionListener(this);
+	 
 	}
 	
-		public static void main(String [] arg)
-	{
+    public void actionPerformed(ActionEvent e) {
+				
+         String a =id.getText();
+         String b =pass.getText();
 		
-		new portal();
+       
+         if(e.getSource()==login2)
+		 {			 
+	    //  if( a=="2"&&b=="1234")
+	     // {
+          new nextpage();
+		  f.setVisible(false);
+		 // }
+		//  else
+		//  {
+		///	  System.out.println("Wrong pass");
+		//  }
+		
 	}
-}	
+	
+         if(e.getSource()==access)
+		 {			 
+	    //  if( a=="2"&&b=="1234")
+	     // {
+          new nextpage();
+		  f.setVisible(false);
+		 // }
+		//  else
+		//  {
+		///	  System.out.println("Wrong pass");
+		//  }
 		
-		
-		
+	}
+	
+}
+}
+	
 	
