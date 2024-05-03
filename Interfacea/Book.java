ed7aaa97bde8cc610107afb97afd7da5ab7bf388
mahinfah <151399467+mahinfah@users.noreply.github.com>
2024-05-03 00:53:36 -0800

    Update Book.java

--- Interfacea/Book.java
+++ Interfacea/Book.java
@@ -1,7 +1,54 @@
-import java.lang*;
+import java.lang.*;
 
 public class Book implements IBookOperations
+{   
+      private int quantity;
+	
+	public Book ( int quantity)
+	{
+		this.quantity=quantity;
+	}
+	
+	public Book()
+	{
+		System.out.println("EMpty cons");
+	}
+	
+	public void setquantity(int quantity)
+	{
+		this.quantity= quantity;
+	}
+	public int getquantity()
+	{
+		return quantity;
+	}
+	public void addBook(int add)
+    {
+    quantity = 	quantity  + add ;
+	
+	}
+	
+	public void sellBook(int sell)
+    {
+	quantity = 	quantity  - sell ;
+		
+	}
+	public void display()
+	{
+		//System.out.println("quantity  :"+ quantity);
+		System.out.println("available books :"+quantity);
+	    //System.out.println("available books");
+	
+	}
+
+
+public static void main(String args [])
 {
 	
+	Book b1 = new Book(20);
+	b1.addBook(5);
+	b1.sellBook(20);
+	b1.display();
+}
 	
 }
